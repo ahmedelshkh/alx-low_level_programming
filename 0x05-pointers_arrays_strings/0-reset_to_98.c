c37648a52b3ff8a1dd9ed8d1bbd6717ab32d43aa
ahmedelshkh <ahmedelshkh6566@gmail.com>
2023-08-21 17:33:12 -0700

    #

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,19 @@
-#include <main.h>
 #include <stdio.h>
 
+void reset_to_98(int *);
+
 /**
+ * main - check the code for Holberton School students.
  *
- * Main - Function that take pointers as a parameter
- *
- * Return : Always 0.
+ * Return: Always 0.
  */
-void reset_to_98(int *n) {
-        *n = 98
+int main(void)
+{
+	int n;
+
+	n = 402;
+	printf("%d\n", n);
+	reset_to_98(&n);
+	printf("%d\n", n);
+	return (0);
 }               
