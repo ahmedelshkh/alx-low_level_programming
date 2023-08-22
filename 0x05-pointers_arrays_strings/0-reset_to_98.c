f316d216c4a39c520e4cf7afd989428947416857
ahmedelshkh <ahmedelshkh6566@gmail.com>
2023-08-21 17:22:31 -0700

    first task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ *
+ * Main - Function that take pointers as a parameter
+ *
+ * Return : Always 0.
+ */
+void reset_to_98(int *n) {
+        *n = 98
+}               
