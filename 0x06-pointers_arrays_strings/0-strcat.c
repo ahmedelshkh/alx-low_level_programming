85a597540180aba2e40f1483484d0458cd32a349
ahmedelshkh <ahmedelshkh6566@gmail.com>
2023-08-23 05:30:47 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+ *
+ * Main - Write a function that concatenates two strings
+ *
+ * Return a pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i,j = 0;
+	while (src[i] != '\0')
+	{
+	dest[j] = src[i];
+	i++;
+	j++;
+	}
+
+	dest[j] = '\0';
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+char *_strcat(char *dest, char *src);
+
+#endif
